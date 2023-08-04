4881f826c5fc396327a530599f77ece3ad5e2f42
Daniel Jeong <dsj58@scarletmail.rutgers.edu>
2023-08-03 21:55:28 -0400

    added sqlite3 database, manga_add database interactions, and manga_view command

--- fubukiBot.py
+++ fubukiBot.py
@@ -1,50 +1,31 @@
-#fubuki bot says hello :D
 import os
 import random
 import asyncio
-
+import sqlite3
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 from utils import mangaSearch
 
+connection = sqlite3.connect("manga_list.db")
+c = connection.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS manga_list (
+            manga_name string NOT NULL PRIMARY KEY,
+            user_id string NOT NULL,
+            manga_link string NOT NULL
+            )""")
+sqlite_insert_with_param = """INSERT INTO manga_list
+                            (user_id, manga_name, manga_link)
+                            VALUES (?, ?, ?);"""
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
-#intents.members = True
-client = discord.Client(intents=intents) #a client is an object that represents a connection to Discord
-
 bot = commands.Bot(command_prefix='!', intents=intents)
-#---------------------------------------------------------------------------------#
-@client.event
-async def on_ready(): #event handler
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name} (id: {guild.id})'  
-          )
-    
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hello {member.name}, welcome to fubuki\'s test server. More features will be implemented very soon, so stay tuned!'
-    )
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
-#---------------------------------------------------------------------------------#
 #help command is not needed, already built into python bots, just need to edit that
 @bot.event
 async def on_ready():
@@ -139,14 +120,22 @@ async def search_for_manga(ctx, *args):
                     print(msg.content)
                     print('got msg')
                     print(f'manga you picked is #{mangaNum}: {search_results[mangaNum][0]}')
-                    await ctx.send(f'You chose manga #{msg.content}: {search_results[mangaNum][0]}')
-                
+                    await ctx.send(f'Manga #{msg.content}: {search_results[mangaNum][0]}, was added to your list')
+
+                    #sqlite stuff here, adding manga into database
+                    user_id = ctx.author.id
+                    manga_name = search_results[mangaNum][0]
+                    manga_link = search_results[mangaNum][1]
+                    insert_values = (user_id, manga_name, manga_link)
+                    c.execute(sqlite_insert_with_param, insert_values)
+                    connection.commit()
+                    break
+
                 else:
                     await message.remove_reaction(reaction, user)
                     # removes reactions if the user tries to go forward on the last page or
                     # backwards on the first page
             except asyncio.TimeoutError:
-                await message.delete()
                 break
                 # ending the loop if user doesn't react after 60 seconds
     else:
@@ -158,5 +147,55 @@ async def remove_manga(ctx):
     #user gives number, and that manga is removed from their list (maybe have Y/N confirmation)
     print()
 
-#client.run(TOKEN) #runs the client, with given token (in this case, fubuki bot)
+@bot.command(name='manga_view', help='view your manga update list')
+async def view_manga(ctx):
+    user_id = ctx.author.id
+    c.execute("SELECT manga_name FROM manga_list WHERE user_id=? ORDER BY manga_name ASC", (user_id,))
+    search_results = c.fetchall()
+    #formats list into pages of 10
+    manga_list = []
+    list_line = []
+    for num, manga in enumerate(search_results, 1):
+        list_line.append(f'{num}. {manga[0]}')
+        if num % 10 == 0:
+            manga_list.append(list_line)
+            list_line = []
+    if list_line:
+        manga_list.append(list_line)#appends any remaining lines left
+    pages = len(manga_list)
+    if pages > 0:
+        curr_page = 1
+        msg_string = "\n".join(manga_list[curr_page-1])
+        message = await ctx.send(f"Page {curr_page}/{pages}:\n{msg_string}")
+        await message.add_reaction("◀️")
+        await message.add_reaction("▶️")
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
+        while True:
+            try:
+                reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
+                # waiting for a reaction to be added - times out after 60 seconds
+
+                if str(reaction.emoji) == "▶️" and curr_page != pages:
+                    curr_page += 1
+                    msg_string = "\n".join(manga_list[curr_page-1])
+                    await message.edit(content=f"Page {curr_page}/{pages}:\n{msg_string}")
+                    await message.remove_reaction(reaction, user)
+
+                elif str(reaction.emoji) == "◀️" and curr_page > 1:
+                    curr_page -= 1
+                    msg_string = "\n".join(manga_list[curr_page-1])
+                    await message.edit(content=f"Page {curr_page}/{pages}:\n{msg_string}")
+                    await message.remove_reaction(reaction, user)
+
+                else:
+                    await message.remove_reaction(reaction, user)
+                    # removes reactions if the user tries to go forward on the last page or
+                    # backwards on the first page
+            except asyncio.TimeoutError:
+                break
+                # ending the loop if user doesn't react after 60 seconds
+    else:
+        await ctx.send(f'Your manga update list is currently empty, try !manga_add *manga_name* to add a manga to your list!')
+    
 bot.run(TOKEN)
